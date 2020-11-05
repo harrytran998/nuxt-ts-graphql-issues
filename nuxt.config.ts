a312302a20cf29ecd5b271fe766fc7651c38c089
Harry Tran <nhattq.coding@gmail.com>
2020-11-05 23:39:12 +0700

    chore: update code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { NuxtConfig } from '@nuxt/types'
 const nuxtConfig: NuxtConfig = {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hello',
+    title: 'Hello bae',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
